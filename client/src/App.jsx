aa654dc1a3d3374c62f1b75d4d43a6e9fdbc047a
Anand Mainali <anand.mainali@cyberarrow.io>
2024-01-16 13:47:15 +0545

    added stripe and payment route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -14,6 +14,7 @@ import ProtectedQuestion from "./components/ProtectedQuestion";
 import {useDispatch} from 'react-redux'
 import axios from 'axios'
 import { questionFetchError, questionFetchStart, questionFetchSuccess } from './redux/question/questionSlice'
+import Payment from "./pages/Payment";
 
 const App = () => {
   const dispatch = useDispatch()
@@ -42,6 +43,7 @@ const App = () => {
           <Route path="/profile/:id" element={<Profile />} />
           <Route path="/add" element={<AddQuestion />} />
           <Route element={<Protected />}>
+            <Route path="/payment" element={<Payment/>} />
             <Route path="/practise" element={<PractiseQuestions />} />
             <Route element={<ProtectedQuestion/>}>
               <Route path="/question/:id" element={<QuestionDetail />} />
