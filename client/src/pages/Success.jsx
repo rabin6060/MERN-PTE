61137f79855c724c3b66a2dafeea2dae76ac0e1a
Anand Mainali <anand.mainali@cyberarrow.io>
2024-01-18 17:26:20 +0545

    some style added

--- client/src/pages/Success.jsx
+++ client/src/pages/Success.jsx
@@ -7,12 +7,14 @@ import {useNavigate} from 'react-router-dom'
 const Success = () => {
   const dispatch = useDispatch()
   const {currentUser} = useSelector(state=>state.user)
-  const sessionId = currentUser.session.id
+  const sessionId = currentUser.sessionId
+  console.log(sessionId);
   const navigate = useNavigate()
   const handleClick =async ()=>{
     dispatch(updateStart())
     try {
       const res = await axios.post('http://localhost:8000/api/user/updateSubsStatus',{sessionId})
+      console.log(res)
       if (!res) {
         updateFailure(res.data)
       }
@@ -22,11 +24,12 @@ const Success = () => {
       console.log(error.message);
       dispatch(updateFailure(error.message))
     }
+    
   }
   return (
-    <div className='h-[60vh]'>
-      <div className='max-w-[60%] w-full h-full m-auto flex items-center py-20'>
-        <div >
+    <div className='h-[80vh]'>
+      <div className='max-w-[60%] w-full h-full m-auto flex items-center justify-center'>
+        <div className='w-full bg-slate-50 space-y-5 p-10 rounded-lg' >
           <h1 className='text-xl sm:text-4xl font-extrabold text-green-700'>Success</h1>
           <button onClick={handleClick} className='btn'>Proceed</button>
         </div>
