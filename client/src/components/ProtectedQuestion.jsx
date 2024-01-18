c6345ce2452832919d78ab90d315093f5a1c9b93
Anand Mainali <anand.mainali@cyberarrow.io>
2024-01-18 18:05:19 +0545

    added some ui

--- client/src/components/ProtectedQuestion.jsx
+++ client/src/components/ProtectedQuestion.jsx
@@ -1,6 +1,6 @@
 import { Outlet,useParams } from "react-router-dom";
 import { useSelector } from "react-redux";
-import Signin from "../pages/Signin";
+import {Link} from 'react-router-dom'
 
 const ProtectedQuestion = () => {
   const { currentUser } = useSelector((state) => state.user);
@@ -14,9 +14,17 @@ const ProtectedQuestion = () => {
     selectedQuestion && selectedQuestion.availability === 'free' ? (
       <Outlet />
     ) : (
-      <div>
-        {/* Render a message or component indicating restricted access */}
-        Access to this question is restricted. Please subscribe.
+      <div className="h-[65vh]">
+        <div className="max-w-[60%] w-full h-full m-auto py-32">
+          <div className="w-full space-y-5">
+            <h1 className="text-xl sm:text-5xl capitalize">Please subscribe to access pro questions.</h1>
+            <p className="text-lg sm:text-3xl text-slate-500 font-[300]">You are required to buy our subscription to fully use our services.</p>
+            <Link to={'/payment'}>
+              <button className="btn px-10 mt-5">Go Pro</button>
+            </Link>
+          
+          </div>
+        </div>
       </div>
     )
   ) : (
