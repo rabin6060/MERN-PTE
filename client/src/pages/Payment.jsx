71298ca4e9f4b90f0aa481b3d0eb1119e7ce4135
Anand Mainali <anand.mainali@cyberarrow.io>
2024-01-18 18:04:43 +0545

    added some style

--- client/src/pages/Payment.jsx
+++ client/src/pages/Payment.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import axios from 'axios'
 import {useSelector,useDispatch} from 'react-redux'
-import { updateSessionFailure, updateSessionStart, updateSessionSuccess } from '../redux/user/userSlice'
+import { updateFailure,updateStart, updateSuccess } from '../redux/user/userSlice'
 
 
 const Payment = () => {
@@ -10,23 +10,28 @@ const Payment = () => {
   const dispatch = useDispatch()
   const handlePayment = async(e) => {
     e.preventDefault()
-    dispatch(updateSessionStart())
-    const res = await axios.post('http://localhost:8000/api/user/create-subs-session',{price:price,customerId:currentUser._id})
-    console.log(res.data);
-    if (!res.data) {
-     dispatch(updateSessionFailure(res.data))
+    dispatch(updateStart())
+    try {
+      const res = await axios.post('http://localhost:8000/api/user/create-subs-session',{price:price,customerId:currentUser._id})
+      if (!res.data) {
+      dispatch(updateFailure(res.data))
+      }
+      dispatch(updateSuccess(res.data.updateUserSession))
+      window.location = res.data.session.url
+    } catch (error) {
+      dispatch(updateFailure(error.message))
     }
-    dispatch(updateSessionSuccess(res.data))
-    window.location = res.data.session.url
+    
   }
   return (
     <div className='w-full h-[80vh]'>
-      <div className='max-w-[60%] w-full h-full m-auto flex items-center justify-center'>
-        <form onSubmit={handlePayment} className='h-max border-2 bg-slate-50 py-14 px-5 space-y-5 rounded-lg'>
+      <div className='max-w-[60%] w-full h-full m-auto flex flex-col py-32 gap-5'>
+        <h1 className='text-xl sm:text-4xl capitalize font-[700]'>Please Subscribe to pro account so you can get all questions.</h1>
+        <form onSubmit={handlePayment} className='w-[50%] h-max border-2 bg-slate-50 py-14 px-5 space-y-5 rounded-lg'>
             <h1 className='text-2xl sm:text-4xl font-[700] '>Subscription Pro</h1>
             <p className='text-[gray] text-2xl'>${price}</p>
             <p className='text-xl font-[300]'>Enjoy All Questions.</p>
-            <button className='btn w-full'>Subscribe</button>
+            <button className='btn px-14'>Subscribe</button>
         </form>
       </div>
     </div>
