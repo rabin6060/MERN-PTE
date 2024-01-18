547a04c191a1d0976557ffdb914d2fe9656ff9b5
Anand Mainali <anand.mainali@cyberarrow.io>
2024-01-18 18:32:49 +0545

    added route for cancel

--- routes/user.route.js
+++ routes/user.route.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import { verifyToken } from '../utils/verifyToken.js'
-import { Delete, Payment, PaymentSuccess, TranscribeAudio, UpdateUser } from '../controllers/user.controller.js'
+import { Delete, Payment, PaymentFailure, PaymentSuccess, TranscribeAudio, UpdateUser } from '../controllers/user.controller.js'
 
 
 const router = express.Router()
@@ -10,5 +10,6 @@ router.delete('/delete/:id',verifyToken,Delete)
 router.post('/audio',verifyToken,TranscribeAudio)
 router.post('/create-subs-session',verifyToken,Payment)
 router.post('/updateSubsStatus',verifyToken,PaymentSuccess)
+router.post('/payment-failed',verifyToken,PaymentFailure)
 
 export default router
