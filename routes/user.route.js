e4d21ae2af47138e43b576418be11c497eab34de
Anand Mainali <anand.mainali@cyberarrow.io>
2024-01-18 16:54:05 +0545

    added payment and subs route

--- routes/user.route.js
+++ routes/user.route.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import { verifyToken } from '../utils/verifyToken.js'
-import { Delete, TranscribeAudio, UpdateUser } from '../controllers/user.controller.js'
+import { Delete, Payment, PaymentSuccess, TranscribeAudio, UpdateUser } from '../controllers/user.controller.js'
 
 
 const router = express.Router()
@@ -8,5 +8,7 @@ const router = express.Router()
 router.put('/update/:id',verifyToken,UpdateUser)
 router.delete('/delete/:id',verifyToken,Delete)
 router.post('/audio',verifyToken,TranscribeAudio)
+router.post('/create-subs-session',verifyToken,Payment)
+router.post('/updateSubsStatus',verifyToken,PaymentSuccess)
 
 export default router
